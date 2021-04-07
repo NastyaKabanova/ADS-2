3c9830de8f721c91c447337bb9ec3530b49ee82c
Nastya Kabanova <76091946+NastyaKabanova@users.noreply.github.com>
2021-04-07 23:43:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,8 @@
 int countPairs1(int *arr, int len, int value) {
   int pairs = 0;
 
-  for (int i = 0; i < len; i++) {
-    for (int j = i; j < len; j++) {
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value)
         pairs++;
     }
