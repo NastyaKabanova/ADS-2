c965b7cf4f83b99ad8685f45bb2cf5031491f93e
Nastya Kabanova <76091946+NastyaKabanova@users.noreply.github.com>
2021-04-07 23:28:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,85 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int pairs = 0;
+  
+  for (int i = 0; i < len; i++) {
+    for (int j = i; j < len; j++) {
+      if (arr[i] + arr[j] == value)
+        pairs++;
+    }
+  }
+  
+  return pairs;
 }
 
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int pairs = 0;
+  int s = 0;
+  int e = len - 1;
+  
+  while (s < e - 1) {
+    int c = (e + s) / 2;
+    
+    if (arr[c] <= value) {
+      s = c + 1;
+    } else {
+      e = c;
+    }
+  }
+  
+  for (int i = 0; i < e; i++) {
+    for (int j = e; j > i; j--) {
+      int sum = arr[i] + arr[j];
+      if (sum == value) {
+        pairs++;
+      } else if (sum < value) {
+        break;
+      } else {
+        continue;
+      }
+    }
+  }
+  
+  return pairs;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int pairs = 0;
+    int s = 0;
+    int e = len - 1;
+    
+    while (s < e - 1) {
+      int center = (s + e) / 2;
+      
+      if (arr[center] <= value) {
+        s = center;
+      } else {
+        e = center;
+      }
+    }
+    
+    int l = e - 1;
+
+    for (int i = 0; i < l; i++) {
+      s = i + 1;
+      e = l - 1;
+      
+      while (s < e) {
+        int center = (s + e) / 2;
+        
+        if (arr[center] < (value - arr[i])) {
+          s = center + 1;
+        } else {
+          e = center;
+        }
+      }
+      
+      while (arr[s] == (value - arr[i])) {
+        pairs++;
+        s++;
+      }
+    }
+    
+    return pairs;
 }
